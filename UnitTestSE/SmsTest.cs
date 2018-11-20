1a84334998d50da83143148e6e1df5f6a92a9144
Maria Luque Anguita <40280156@live.napier.ac.uk>
2018-11-20 14:02:08 +0000

    test started

---
+++ UnitTestSE/SmsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SE_cw1_maria;
+
+namespace UnitTestSE
+{
+    [TestClass]
+    public class SmsTest
+    {
+        Sms sms = new Sms();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CountryCodeFail()
+        {
+            sms.CountryCode = "12345";
+        }
+
+        [TestMethod]
+        public void ContryCodePass()
+        {
+            sms.CountryCode = "+34";
+            string expected = "+34";
+
+            Assert.AreEqual(expected, sms.CountryCode);
+        }
+    }
+}
